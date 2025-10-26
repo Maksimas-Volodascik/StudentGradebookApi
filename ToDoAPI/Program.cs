4327c1889331da307e7dc17bcc46d01795a6b65a
max <maksimas.volodascik@gmail.com>
2025-10-26 18:26:18 +0200

    Added JWT, Login and Register requests

--- ToDoAPI/Program.cs
+++ ToDoAPI/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.IdentityModel.Tokens;
+using Scalar.AspNetCore;
 using System.Text;
 using ToDoAPI.Data;
 using ToDoAPI.Mappings;
@@ -16,34 +17,6 @@ namespace ToDoAPI
         public static void Main(string[] args)
         {
             var builder = WebApplication.CreateBuilder(args);
-
-            builder.Services.AddSingleton<TokenService>();
-            builder.Services.AddAuthentication(options =>
-            {
-                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            })
-            .AddJwtBearer(options =>
-            {
-                var key = builder.Configuration["Jwt:Key"];
-                var issuer = builder.Configuration["Jwt:Issuer"];
-                var audience = builder.Configuration["Jwt:Audience"];
-
-                options.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateIssuer = true,
-                    ValidIssuer = issuer,
-
-                    ValidateAudience = true,
-                    ValidAudience = audience,
-
-                    ValidateIssuerSigningKey = true,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(key)),
-
-                    ValidateLifetime = true,
-                    ClockSkew = TimeSpan.FromSeconds(30)
-                };
-            });
 
             builder.Services.AddAuthorization();
 
@@ -65,6 +38,7 @@ namespace ToDoAPI
             if (app.Environment.IsDevelopment())
             {
                 app.MapOpenApi();
+                app.MapScalarApiReference();
             }
 
             app.UseHttpsRedirection();
--- ToDoAPI/Services/StudentService.cs
+++ ToDoAPI/Services/StudentService.cs
@@ -1,4 +1,11 @@
-﻿using ToDoAPI.Models;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using ToDoAPI.DTOs;
+using ToDoAPI.Models;
 using ToDoAPI.Repositories;
 
 namespace ToDoAPI.Services
@@ -6,23 +13,73 @@ namespace ToDoAPI.Services
     public class StudentService : IStudentService
     {
         private readonly IRepository<Students> _studentRepository;
-        public StudentService(IRepository<Students> studentRepository)
+        private readonly IConfiguration _configuration;
+        public StudentService(IConfiguration configuration, IRepository<Students> studentRepository)
         {
+            _configuration = configuration;
             _studentRepository = studentRepository;
         }
-
         public async Task<IEnumerable<Students>> GetAllStudentsAsync() =>
             await _studentRepository.GetAllAsync();
         public async Task<Students?> GetStudentByIdAsync(int id) =>
             await _studentRepository.GetByIdAsync(id);
-        public async Task AddStudentAsync(Students student){
-            await _studentRepository.AddAsync(student);
-            await _studentRepository.SaveChangesAsync();
-        }
         public async Task DeleteStudentAsync(Students student)
         {
             _studentRepository.Delete(student);
             await _studentRepository.SaveChangesAsync();
+        }
+        public async Task<Students?> RegisterAsync(StudentData studentData)
+        {
+            if (await _studentRepository.GetByEmailAsync(studentData.email) != null)
+            {
+                return null; //User exists
+            }
+            var student = new Students();
+            var passwordHasher = new PasswordHasher<Students>();
+            var hashedPassword = passwordHasher.HashPassword(student, studentData.password);
+            student.first_name = studentData.first_name;
+            student.last_name = studentData.last_name;
+            student.email = studentData.email;
+            student.passwordHash = hashedPassword;
+            student.status = studentData.status;
+
+            await _studentRepository.AddAsync(student);
+            await _studentRepository.SaveChangesAsync();
+
+            return student;
+        }
+        public async Task<string?> LoginAsync(StudentData studentData)
+        {
+            var student = await _studentRepository.GetByEmailAsync(studentData.email);
+            if (student is null || new PasswordHasher<Students>().VerifyHashedPassword(student, student.passwordHash, studentData.password) == PasswordVerificationResult.Failed)
+            {
+                return null;
+            }
+
+            return CreateToken(student);
+
+        }
+        private string CreateToken(Students students)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Email, students.email),
+                new Claim(ClaimTypes.NameIdentifier, students.student_id.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(
+                Encoding.UTF8.GetBytes(_configuration.GetValue<string>("AppSettings:Token")!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: _configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: _configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
         }
     }
 }
