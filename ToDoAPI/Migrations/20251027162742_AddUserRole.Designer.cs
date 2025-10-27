a3d3d5c947ba07df2be030056fb45b434e99b7cb
max <maksimas.volodascik@gmail.com>
2025-10-27 18:54:20 +0200

    Added RefreshToken

--- ToDoAPI/Migrations/20251027162742_AddUserRole.Designer.cs
+++ ToDoAPI/Migrations/20251027162742_AddUserRole.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using ToDoAPI.Data;
 
@@ -11,9 +12,11 @@ using ToDoAPI.Data;
 namespace ToDoAPI.Migrations
 {
     [DbContext(typeof(SchoolDbContext))]
-    partial class SchoolDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251027162742_AddUserRole")]
+    partial class AddUserRole
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -145,12 +148,6 @@ namespace ToDoAPI.Migrations
                     b.Property<string>("passwordHash")
                         .IsRequired()
                         .HasColumnType("nvarchar(max)");
-
-                    b.Property<string>("refreshToken")
-                        .HasColumnType("nvarchar(max)");
-
-                    b.Property<DateTime?>("refreshTokenExpiryTime")
-                        .HasColumnType("datetime2");
 
                     b.Property<string>("role")
                         .IsRequired()
