7490701e1f12463c1db5bbe501b3387f74ae9e78
max <maksimas.volodascik@gmail.com>
2025-12-15 21:57:16 +0200

    Added Users table to the database

---
+++ ToDoAPI/Models/WebUsers.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoAPI.Models
+{
+    public class WebUsers
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string PasswordHash { get; set; }
+        public string? RefreshToken { get; set; }
+        public DateTime? RefreshTokenExpiryTime { get; set; }
+        public string Role { get; set; } = "Demo";
+        public Teachers Teachers { get; set; } = null!;
+        public Students Students { get; set; } = null!;
+    }
+}
