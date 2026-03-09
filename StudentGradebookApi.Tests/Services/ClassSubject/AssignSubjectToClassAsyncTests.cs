61bdc1ec6eb55431ed8ecd53cc7089e05dace779
max <maksimas.volodascik@gmail.com>
2026-03-09 17:53:22 +0200

    feat(tests): add classSubject unit testing

--- StudentGradebookApi.Tests/Services/ClassSubject/AssignSubjectToClassAsyncTests.cs
+++ StudentGradebookApi.Tests/Services/ClassSubject/AssignSubjectToClassAsyncTests.cs
@@ -1,4 +1,13 @@
-﻿using System;
+﻿using Moq;
+using StudentGradebookApi.DTOs.ClassSubjects;
+using StudentGradebookApi.Models;
+using StudentGradebookApi.Repositories.ClassSubjectsRepository;
+using StudentGradebookApi.Services.ClassesServices;
+using StudentGradebookApi.Services.SubjectClassServices;
+using StudentGradebookApi.Services.SubjectsService;
+using StudentGradebookApi.Services.TeacherServices;
+using StudentGradebookApi.Shared;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,6 +17,93 @@ namespace StudentGradebookApi.Tests.Services.ClassSubject
 {
     public class AssignSubjectToClassAsyncTests
     {
+        private readonly Mock<IClassSubjectsRepository> _classSubjectsRepositoryMock;
+        private readonly Mock<ITeacherService> _teacherService;
+        private readonly Mock<ISubjectsService> _subjectsService;
+        private readonly Mock<IClassesServices> _classesService;
+        private readonly ClassSubjectsService _classSubjectsService;
 
+        public AssignSubjectToClassAsyncTests()
+        {
+            _classSubjectsRepositoryMock = new Mock<IClassSubjectsRepository>();
+            _teacherService = new Mock<ITeacherService>();
+            _subjectsService = new Mock<ISubjectsService>();
+            _classesService = new Mock<IClassesServices>();
+
+            _classSubjectsService = new ClassSubjectsService(_classSubjectsRepositoryMock.Object, _teacherService.Object, _subjectsService.Object, _classesService.Object);
+        }
+
+        [Fact]
+        public async Task AssignSubjectToClassAsync_ValidData_ReturnsSuccessResult()
+        {
+            var subject = new Subjects{ Id = 1 };
+            var classes = new Classes{ Id = 1 };
+            var dto = new CombineClassSubjectDTO
+            {
+                SubjectId = subject.Id,
+                ClassId = classes.Id
+            };
+
+            _subjectsService.Setup(s => s.GetSubjectByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(Result<Subjects>.Success(subject));
+
+            _classesService.Setup(s => s.GetClassByIdAsync(It.IsAny<int>()))
+                .ReturnsAsync(Result<Classes>.Success(classes));
+
+            var result = await _classSubjectsService.AssignSubjectToClassAsync(dto);
+
+            Assert.True(result.IsSuccess);
+            _classSubjectsRepositoryMock.Verify(c => c.AddAsync(It.Is<ClassSubjects>(cs => 
+            cs.SubjectId == subject.Id && cs.ClassId == classes.Id)), Times.Once);
+            _classSubjectsRepositoryMock.Verify(c => c.SaveChangesAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task AssignSubjectToClassAsync_SubjectNotFound_ReturnsFailure()
+        {
+            var dto = new CombineClassSubjectDTO
+            {
+                SubjectId = 1,
+                ClassId = 1
+            };
+
+            var error = Errors.SubjectErrors.SubjectNotFound;
+
+            _subjectsService.Setup(s => s.GetSubjectByIdAsync(dto.SubjectId))
+                .ReturnsAsync(Result<Subjects>.Failure(error));
+
+            var result = await _classSubjectsService.AssignSubjectToClassAsync(dto);
+
+            Assert.False(result.IsSuccess);
+            Assert.Equal(error, result.Error);
+            _classSubjectsRepositoryMock.Verify(r => r.AddAsync(It.IsAny<ClassSubjects>()),Times.Never);
+            _classSubjectsRepositoryMock.Verify(r => r.SaveChangesAsync(), Times.Never);
+        }
+
+        [Fact]
+        public async Task AssignSubjectToClassAsync_ClassNotFound_ReturnsFailure()
+        {
+            var dto = new CombineClassSubjectDTO
+            {
+                SubjectId = 1,
+                ClassId = 1
+            };
+
+            var subject = new Subjects { Id = 1 };
+            var error = Errors.ClassesErrors.ClassNotFound;
+
+            _subjectsService.Setup(s => s.GetSubjectByIdAsync(dto.SubjectId))
+                .ReturnsAsync(Result<Subjects>.Success(subject));
+
+            _classesService.Setup(s => s.GetClassByIdAsync(dto.ClassId))
+                .ReturnsAsync(Result<Classes>.Failure(error));
+
+            var result = await _classSubjectsService.AssignSubjectToClassAsync(dto);
+
+            Assert.False(result.IsSuccess);
+            Assert.Equal(error, result.Error);
+            _classSubjectsRepositoryMock.Verify(r => r.AddAsync(It.IsAny<ClassSubjects>()), Times.Never);
+            _classSubjectsRepositoryMock.Verify(r => r.SaveChangesAsync(), Times.Never);
+        }
     }
 }
---
+++ StudentGradebookApi.Tests/Services/ClassSubject/EditSubjectClassTeacher.cs
@@ -0,0 +1,97 @@
+﻿using Moq;
+using StudentGradebookApi.Models;
+using StudentGradebookApi.Repositories.ClassSubjectsRepository;
+using StudentGradebookApi.Services.ClassesServices;
+using StudentGradebookApi.Services.SubjectClassServices;
+using StudentGradebookApi.Services.SubjectsService;
+using StudentGradebookApi.Services.TeacherServices;
+using StudentGradebookApi.Shared;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentGradebookApi.Tests.Services.ClassSubject
+{
+    public class EditSubjectClassTeacher
+    {
+        private readonly Mock<IClassSubjectsRepository> _classSubjectsRepositoryMock;
+        private readonly Mock<ITeacherService> _teacherService;
+        private readonly Mock<ISubjectsService> _subjectsService;
+        private readonly Mock<IClassesServices> _classesService;
+        private readonly ClassSubjectsService _classSubjectsService;
+
+        public EditSubjectClassTeacher()
+        {
+            _classSubjectsRepositoryMock = new Mock<IClassSubjectsRepository>();
+            _teacherService = new Mock<ITeacherService>();
+            _subjectsService = new Mock<ISubjectsService>();
+            _classesService = new Mock<IClassesServices>();
+
+            _classSubjectsService = new ClassSubjectsService(_classSubjectsRepositoryMock.Object, _teacherService.Object, _subjectsService.Object, _classesService.Object);
+        }
+
+        [Fact]
+        public async Task EditSubjectClassTeacher_ValidData_ReturnsSuccess()
+        {
+            int teacherId = 1;
+            int classSubjectId = 10;
+            var teacher = new Teachers { Id = teacherId };
+            var classSubject = new ClassSubjects { Id = classSubjectId };
+
+            _teacherService.Setup(x => x.GetTeacherByIdAsync(teacherId))
+                .ReturnsAsync(Result<Teachers>.Success(teacher));
+
+            _classSubjectsRepositoryMock.Setup(x => x.GetByIdAsync(classSubjectId))
+                .ReturnsAsync(classSubject);
+
+            var result = await _classSubjectsService.EditSubjectClassTeacher(classSubjectId, teacherId);
+
+            Assert.True(result.IsSuccess);
+            _classSubjectsRepositoryMock.Verify(r => r.Update(It.Is<ClassSubjects>(cs =>
+                    cs.TeacherId == teacherId && cs.Teachers == teacher)), Times.Once);
+            _classSubjectsRepositoryMock.Verify(r => r.SaveChangesAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task EditSubjectClassTeacher_TeacherNotFound_ReturnsFailure()
+        {
+            int teacherId = 1;
+            int classSubjectId = 10;
+            var error = Errors.TeacherErrors.TeacherNotFound;
+
+            _teacherService.Setup(x => x.GetTeacherByIdAsync(teacherId))
+                .ReturnsAsync(Result<Teachers>.Failure(error));
+
+            var result = await _classSubjectsService.EditSubjectClassTeacher(classSubjectId, teacherId);
+
+            Assert.False(result.IsSuccess);
+            Assert.Equal(error, result.Error);
+            _classSubjectsRepositoryMock.Verify(r => r.Update(It.IsAny<ClassSubjects>()), Times.Never);
+            _classSubjectsRepositoryMock.Verify(r => r.SaveChangesAsync(), Times.Never);
+        }
+
+        [Fact]
+        public async Task EditSubjectClassTeacher_ClassSubjectNotFound_ReturnsFailure()
+        {
+            int teacherId = 1;
+            int classSubjectId = 10;
+            var teacher = new Teachers { Id = teacherId };
+            var error = Errors.ClassSubjectErrors.ClassSubjectNotFound;
+
+            _teacherService.Setup(x => x.GetTeacherByIdAsync(teacherId))
+                .ReturnsAsync(Result<Teachers>.Success(teacher));
+
+            _classSubjectsRepositoryMock.Setup(x => x.GetByIdAsync(classSubjectId))
+                .ReturnsAsync((ClassSubjects)null);
+
+            var result = await _classSubjectsService.EditSubjectClassTeacher(classSubjectId, teacherId);
+
+            Assert.False(result.IsSuccess);
+            Assert.Equal(error, result.Error);
+            _classSubjectsRepositoryMock.Verify(r => r.Update(It.IsAny<ClassSubjects>()), Times.Never);
+            _classSubjectsRepositoryMock.Verify(r => r.SaveChangesAsync(), Times.Never);
+        }
+    }
+}
