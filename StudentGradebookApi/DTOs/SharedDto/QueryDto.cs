c4009f205d246d878c22bbdb271970b7630165f6
max <maksimas.volodascik@gmail.com>
2026-03-15 18:41:51 +0200

    feat(api): added pagination to get calls

---
+++ StudentGradebookApi/DTOs/SharedDto/QueryDto.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentGradebookApi.DTOs.SharedDto
+{
+    public class QueryDto
+    {
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 20;
+        public int MaxSize = 100;
+        public int ValidPageSize
+        {
+            
+            get { return (PageSize > MaxSize || PageSize < 1) ? 20 : PageSize; }
+        }
+        //sortBy
+        //sortDirection =asc/desc
+
+        //filter by
+    }
+}
